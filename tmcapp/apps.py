12ac921a2ef24974e6689c6fc738781811a48d18
vorellan2 <mastermindg02@gmail.com>
2020-06-21 00:14:43 -0400

    add app and model

---
+++ tmcapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TmcappConfig(AppConfig):
+    name = 'tmcapp'
---
+++ tmcapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Credito(models.Model):
+    monto_uf = models.DecimalField(max_digits=5, decimal_places=2)
+    plazo = models.IntegerField(blank=True, null=False)
+    fecha = models.DateTimeField("Fecha", auto_now_add=False)
